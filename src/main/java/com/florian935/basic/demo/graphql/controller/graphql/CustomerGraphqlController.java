a4ba0db893f713c78edad6bd0fb77e956871e3d2
florian935 <florian.martin63000@gmail.com>
2022-07-02 15:04:32 +0200

    Demo of basics finished

--- src/main/java/com/florian935/basic/demo/graphql/controller/graphql/CustomerGraphqlController.java
+++ src/main/java/com/florian935/basic/demo/graphql/controller/graphql/CustomerGraphqlController.java
@@ -1,15 +1,21 @@
 package com.florian935.basic.demo.graphql.controller.graphql;
 
 import com.florian935.basic.demo.graphql.domain.Customer;
+import com.florian935.basic.demo.graphql.domain.CustomerInput;
 import com.florian935.basic.demo.graphql.domain.Order;
 import com.florian935.basic.demo.graphql.domain.Product;
 import com.florian935.basic.demo.graphql.repository.CustomerRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 @Controller
@@ -19,6 +25,11 @@ public class CustomerGraphqlController {
 
     public CustomerGraphqlController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+    }
+
+    @QueryMapping
+    Mono<Customer> customerById(@Argument Integer customerId) {
+        return this.customerRepository.findById(customerId).switchIfEmpty(Mono.just(new Customer(100, "NULL")));
     }
 
     @QueryMapping
@@ -44,5 +55,27 @@ public class CustomerGraphqlController {
                 .toList();
 
         return Flux.fromIterable(products);
+    }
+
+    @MutationMapping
+    Mono<Customer> addCustomer(@Argument String name) {
+        return this.customerRepository.save(new Customer(null, name));
+    }
+
+    @MutationMapping
+    Mono<Customer> addCustomerInput(@Argument CustomerInput customerInput) {
+        return this.customerRepository.save(new Customer(null, customerInput.name()));
+    }
+
+    @MutationMapping
+    Mono<Boolean> deleteCustomerById(@Argument Integer customerId) {
+        return this.customerRepository.findById(customerId)
+                .map(Customer::customerId)
+                .flatMap(id -> {
+                    this.customerRepository.deleteById(id);
+
+                    return Mono.just(true);
+                })
+                .defaultIfEmpty(false);
     }
 }
---
+++ src/main/java/com/florian935/basic/demo/graphql/domain/CustomerInput.java
@@ -0,0 +1,4 @@
+package com.florian935.basic.demo.graphql.domain;
+
+public record CustomerInput(Integer customerId, String name) {
+}
